c4ef744c27bff99a9759ca32d7bce6e39af136ee
Serge Markov <serge.markov+git@gmail.com>
2018-03-04 23:52:52 +0700

    Gradle changes + InstanceId service

---
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkerFirebaseInstanceIdService.java
@@ -0,0 +1,44 @@
+package android.example.com.squawker.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by mserge on 04.03.2018.
+ */
+// TODO (1) Make a new package for your FCM service classes called "fcm"
+// TODO (2) Create a new Service class that extends FirebaseInstanceIdService.
+// You'll need to implement the onTokenRefresh method. Simply have it print out
+// the new token.
+
+public class SquawkerFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    private static final String TAG = "SquawkerFIIS";
+
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+    // [END refresh_token]
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+}
